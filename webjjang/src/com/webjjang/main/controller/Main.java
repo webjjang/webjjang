757f25049d7d7fdaf106e77afb556cc21e46d1aa
webjjang <leelj1@nate.com>
2026-01-21 23:06:54 +0900

    웹짱 초기 프로젝트

---
+++ webjjang/src/com/webjjang/main/controller/Main.java
@@ -0,0 +1,11 @@
+package com.webjjang.main.controller;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Main");
+	}
+
+}
